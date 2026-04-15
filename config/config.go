9e5ee77b9ae93f19bc869409ba2959f3b491487e
Sverre H. Huseby <shh@thathost.com>
2026-04-15 16:03:14 +0200

    foo

--- config/config.go
+++ config/config.go
@@ -10,21 +10,37 @@ import (
 type Config struct {
 	// fields need to have capitalized names to be unmarshalled properly.
 	// in the YAML file, the corresponding keys may be lowercase.
-	Port int
-	Host string
+	Port  int
+	Host  string
+	Repos []struct {
+		Name  string
+		Token string
+	}
 }
 
 func LoadConfig() (Config, error) {
-	yamlContents, err := os.ReadFile("config.yaml")
+	config := Config{}
+	err := loadConfigInto(&config, "config.default.yaml")
 	if err != nil {
 		return Config{}, err
 	}
-	var config Config
-	err = yaml.Unmarshal(yamlContents, &config)
+	err = loadConfigInto(&config, "config.yaml")
 	if err != nil {
 		return Config{}, err
 	}
 	return config, nil
+}
+
+func loadConfigInto(config *Config, filename string) error {
+	yamlContents, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	err = yaml.Unmarshal(yamlContents, config)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
