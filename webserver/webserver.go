13ba07fe635f78d3de05fe8c1c2219a91e3f41cf
Sverre H. Huseby <shh@thathost.com>
2026-08-06 10:39:57 +0200

    chore: add Prometheus counter

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -6,17 +6,36 @@ import (
 	"strconv"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type indexHandler struct{}
 
+type metrics struct {
+	numRequests prometheus.Counter
+}
+
+var m *metrics
+
+func newMetrics(reg prometheus.Registerer) *metrics {
+	m := &metrics{
+		numRequests: promauto.With(reg).NewCounter(prometheus.CounterOpts{
+			Name: "webserver_requests_total",
+			Help: "Total number of requests received by the webserver",
+		}),
+	}
+	return m
+}
+
 func (h *indexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, Go webserver!"))
+	w.Write([]byte("Hello, Go webserver!\n"))
+	m.numRequests.Inc()
 }
 
 func main() {
 	reg := prometheus.NewRegistry()
+	m = newMetrics(reg)
 	port := 8086
 	mux := http.NewServeMux()
 	mux.Handle("/", &indexHandler{})
