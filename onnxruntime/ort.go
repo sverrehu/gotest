e1d207e91620d1d7defa3ba90c16dd6e4815d0e0
Sverre H Huseby <shh@thathost.com>
2026-09-16 21:35:41 +0200

    foo

--- onnxruntime/ort.go
+++ onnxruntime/ort.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"image"
 	"log"
+	"os"
 	"runtime"
+
+	"image/draw"
 
 	ort "github.com/yalue/onnxruntime_go"
 )
@@ -34,4 +38,81 @@ func main() {
 		panic(e)
 	}
 	defer ort.DestroyEnvironment()
+	modelPath := "yolo26_face_fp16.onnx"
+	//modelPath := "yolov8n-face.onnx"
+	inputPath := "../born/faces.jpg"
+	outputPath := "/tmp/faces_annotated.jpg"
+
+	inputTensor, rgbaImg, err := loadAndProcessImage(inputPath)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer inputTensor.Destroy()
+
+	// Prepare empty output tensor shape allocation
+	// Standard YOLO outputs are [1, 84, 8400] -> 84 channels (4 box coordinates + 80 classes), 8400 boxes
+	outputShape := ort.NewShape(1, 84, 8400)
+	outputData := make([]float32, 1*84*8400)
+	outputTensor, err := ort.NewTensor(outputShape, outputData)
+	if err != nil {
+		panic(err)
+	}
+	defer outputTensor.Destroy()
+
+	session, err := ort.NewAdvancedSession(modelPath,
+		[]string{"images"}, []string{"output0"}, // Tensor node names inside the ONNX model
+		[]ort.ArbitraryTensor{inputTensor}, []ort.ArbitraryTensor{outputTensor}, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer session.Destroy()
+
+	err = session.Run()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func loadAndProcessImage(filePath string) (*ort.Tensor[float32], *image.RGBA, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer file.Close()
+
+	src, _, err := image.Decode(file)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	bounds := src.Bounds()
+	rgbaImg := image.NewRGBA(bounds)
+	draw.Draw(rgbaImg, bounds, src, bounds.Min, draw.Src)
+
+	// Pre-allocate planar CHW float32 array (3 channels * 640 height * 640 width)
+	data := make([]float32, 3*640*640)
+
+	offsetX := 0
+	offsetY := 0
+
+	for y := 0; y < 640; y++ {
+		for x := 0; x < 640; x++ {
+			r, g, b, _ := rgbaImg.At(offsetX+x, offsetY+y).RGBA()
+
+			idxR := 0*640*640 + y*640 + x
+			idxG := 1*640*640 + y*640 + x
+			idxB := 2*640*640 + y*640 + x
+
+			data[idxR] = float32(r) / 65535.0
+			data[idxG] = float32(g) / 65535.0
+			data[idxB] = float32(b) / 65535.0
+		}
+	}
+
+	inputShape := ort.NewShape(1, 3, 640, 640)
+	t, err := ort.NewTensor(inputShape, data)
+	if err != nil {
+		return nil, nil, err
+	}
+	return t, rgbaImg, nil
 }
